4573e15bac7a2f6aaf8ed124477afca8707bbbc0
Chris Palmer <christopherpalmer283@gmail.com>
2023-06-14 16:41:14 +0100

    Added content to page for user to learn about me. fixed bugs where questions would display correct answer

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # AboutMe
 
+website: with infomation about me and a guessing game to test what they have learnt from the page. 
 
 ![accessability score!](/accessibility.png "Accessability score")
--- app.js
+++ app.js
@@ -6,64 +6,105 @@ let animal;
 let answer;
 let myJoke;
 let who;
-
+let quiz;
+let answer0;
+let answer1;
+let answer2;
+let answer3;
+let answer4;
 
 function myFunction() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
- }
-
-alert("Hello, Welcome to my site");
-
-function userName(){
-
-    user = prompt("Lets get to know each other better; lets start with telling me your name");
-    // console.log(user);
-    alert(" Great i can already tell we are going to be best friends " + user);
+  let element = document.body;
+  element.classList.toggle("dark-mode");
 }
 
-userName();
+// alert("Hello, Welcome to my site");
 
-function favAnimal(){
+// function userName() {
+//   user = prompt(
+//     "Lets get to know each other better; lets start with telling me your name"
+//   );
+//   // console.log(user);
+//   alert(" Great i can already tell we are going to be best friends " + user);
+// }
 
-    animal = prompt(user + " what is your favourite Animal?").toLowerCase();
-    console.log(animal);
+// userName();
 
-if(animal === " dog"){
-myFavAnimal = " Cat"
-} else if (animal === " Cat"){
-    myFavAnimal = " pangolin"
-} else {
-    myFavAnimal = " Liger"
-}
-alert("Cool " + animal + " is an anamzing animal. My favourite animal is pretty much a" + myFavAnimal);
-    
-}
+// function favAnimal() {
+//   animal = prompt(" what is your favourite Animal?");
+//   console.log(animal);
 
-favAnimal();
+//   if (animal == "dog") {
+//     myFavAnimal = "cat";
+//     console.log(myFavAnimal);
+//   } else if (animal == "cat") {
+//     myFavAnimal = "pangolin";
+//     console.log(myFavAnimal);
+//   } else {
+//     myFavAnimal = "Liger";
+//     console.log(myFavAnimal);
+//   }
+//   alert(
+//     "Cool " +
+//       animal +
+//       " is an amzing animal. My favourite animal is pretty much a " +
+//       myFavAnimal
+//   );
+// }
 
-function joke(){
+// favAnimal();
 
-    answer = prompt(user + ", would you like to hear a joke?").toLowerCase()
-    console.log(answer);
+// function joke() {
+//   answer = prompt(user + ", would you like to hear a joke?");
+//   console.log(answer);
 
-    if (answer === "yes"){
-        myJoke = prompt("knock, knock")
+//   if (answer === "yes") {
+//     myJoke = prompt("knock, knock");
 
-if (myJoke === "whos there"){
-    who = prompt("disk")
-    }
-    if (who === "disk who"){
-                alert("Disk is a recording, please leave your message after the beep!");
-    }
-    } 
-}
+//     if (myJoke === "whos there") {
+//       who = prompt("disk");
+//     }
+//     if (who === "disk who") {
+//       alert("Disk is a recording, please leave your message after the beep!");
+//     }
+//   }
+// }
 
-joke();
+// joke();
 
-alert(" it feels like ive know you a lifetime already " + " your name is " +  user + ", your favourite animal is a " +  animal + ", and you love my jokes");
+// alert(
+//   " it feels like ive know you a lifetime already " +
+//     " your name is " +
+//     user +
+//     ", your favourite animal is a " +
+//     animal +
+//     ", and you love my jokes"
+// );
 
-alert("now i know about you " + user + ", its time for you to learn about me; please read my site to learn more,when you think you know me better have a go at the quiz, thank you =]");
+// alert(
+//   "now i know about you " +
+//     user +
+//     ", its time for you to learn about me; please read my site to learn more,"
+// );
 
+// alert(
+//   "So; you think you know me now do you well,  i hope you do as your life depends on it"
+// );
+// alert("Mwhahahaha!");
+// alert("please answer all questions yes/y or no/n");
 
+// function q0() {
+//   answer0 = prompt("Question 1: Am i 33 years old?");
+//   console.log(answer0);
 
+//   if (answer0 === "yes" || answer0 === "y") {
+//     alert("Did you even read the Bio, im 33 years young!");
+//     console.log(answer0);
+//   } else if (answer0 === "no" || answer0 === "n") {
+//     alert("Correct, i am very youthful");
+//     console.log(answer0);
+//   } else {
+//     alert("Thats not an answer");
+//   }
+// }
+// q0();
